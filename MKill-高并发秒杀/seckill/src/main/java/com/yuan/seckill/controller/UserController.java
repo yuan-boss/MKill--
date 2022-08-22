c94914c761aa766b3dea91e0d8141b33664ac2f1
yuanBoss <1928627476@qq.com>
2022-08-22 17:39:42 +0800

    系统压测

--- MKill-高并发秒杀/seckill/src/main/java/com/yuan/seckill/controller/UserController.java
+++ MKill-高并发秒杀/seckill/src/main/java/com/yuan/seckill/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.yuan.seckill.controller;
 
 
+import com.yuan.seckill.entity.User;
+import com.yuan.seckill.vo.RespBean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,4 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    /**
+     * @Author yuan_boss
+     * @Description 测试用户信息
+     * @Date 13:49 2022/8/22
+     * @Param
+     * @return
+     **/
+    @RequestMapping("/info")
+    @ResponseBody
+    public RespBean info(User user){
+        return RespBean.success(user);
+    }
 }
